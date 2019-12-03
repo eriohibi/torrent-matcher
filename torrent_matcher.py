650b1c96aaae9244e0916c19ffff3ee7db8f6927
eriohibi <eriohibi-27@protonmail.ch>
2019-12-03 15:46:35 +1300

    Check whether given torrent matches local files

--- torrent_matcher.py
+++ torrent_matcher.py
@@ -2,6 +2,8 @@
 #     and finds which torrents have matching local files.
 
 import os
+import sys
+import torrent_parser as tp
 
 # Load from file a list of directories to check for files
 directories = open('directories.txt', 'r').readlines()
@@ -20,3 +22,27 @@ for d in directories:
             })
 
 print('Done')
+
+# Get from program arguments the torrent file to check
+# TODO: Support a collection of torrent files
+if len(sys.argv) < 2:
+    print('No arguments given')
+    exit()
+
+torrent_file = sys.argv[1]
+torrent_info = tp.parse_torrent_file(torrent_file)
+
+# Check whether the torrent matches up with any local files
+# TODO: Check whether path matches as well, not just filename.
+#       And perhaps also file size and/or hash if possible
+matches = 0
+total = len(torrent_info['info']['files'])
+
+for f in torrent_info['info']['files']:
+    if f['path'][-1] in [x['filename'] for x in files]:
+        matches += 1
+
+print(f'Matches: {matches}/{total}')
+
+# If every file in the torrent is matched with a local one, we have a complete
+#     torrent detected
